662c7ef49f309f354e96f6ef89bee1f3c381c775
arekbee_cp <SND\arekbee_cp>
2011-02-03 15:21:12 +0000

    First release :) Have fun

---
+++ PdfLittleSigner/PdfLittleSignerSampel/Program.cs
@@ -0,0 +1,15 @@
+﻿using PdfLttleSigner;
+
+namespace PdfLittleSignerSampel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetaData metadata = new MetaData();
+            metadata.Author = "me";
+            PdpSigner signer = new PdpSigner("CertyficateName", "file.pdf", "file_signed.pdf", metadata);
+            bool signed = signer.Sign("reasonToSign", "Contact", "PhysicalLocation", true, "image.jpg");
+        }
+    }
+}
