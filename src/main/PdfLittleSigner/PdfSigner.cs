f77df2acb7d81a6b0f3b18e0cfdd4a9a83dbcc44
Marcin Flądro <marcin.fladro@matrix-reliability.eu>
2022-07-19 13:51:24 +0200

    changed signature position, throw exception if stamp image format is unsupported

--- src/main/PdfLittleSigner/PdfSigner.cs
+++ src/main/PdfLittleSigner/PdfSigner.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
+using Rectangle = iText.Kernel.Geom.Rectangle;
 
 namespace PdfLittleSigner
 {
@@ -23,8 +24,9 @@ namespace PdfLittleSigner
         private readonly string _outputPdfFileString = "";
         private Stream _outputPdfStream;
         private readonly Size _imageSize = new(248, 99);
-        private readonly Size _imageLocation = new(60, 100);
         private readonly ILogger<PdfSigner> _logger;
+        private string[] _unsupportedImageFormats = { ".png" };
+        private const int _stampMargin = 5;
 
         #endregion
 
@@ -58,13 +60,7 @@ namespace PdfLittleSigner
                 GetChain(certificate)
                 : throw new CryptographicException("Certificate is NULL. Certificate can not be found");
 
-            _logger.Log(LogLevel.Information, "Reading pdf file...");
-
-            await using Stream inputPdfFile = new MemoryStream(fileToSign);
-            PdfReader pdfReader = new(inputPdfFile);
-
             _logger.Log(LogLevel.Information, "Creating output stream...");
-
             if (_outputPdfStream == null && !string.IsNullOrEmpty(_outputPdfFileString))
             {
                 _outputPdfStream = new FileStream(_outputPdfFileString, FileMode.OpenOrCreate, FileAccess.Write);
@@ -75,12 +71,20 @@ namespace PdfLittleSigner
                 return false;
             }
 
+            _logger.Log(LogLevel.Information, "Reading pdf file...");
+            await using Stream inputPdfSigner = new MemoryStream(fileToSign);
+            using PdfReader singerPdfReader = new(inputPdfSigner);
+
             try
             {
-                var pdfSigner = GetPdfSigner(pdfReader);
+                var pdfSigner = GetPdfSigner(singerPdfReader);
 
                 _logger.Log(LogLevel.Information, "Setting signature appearance...");
-                await ConfigureSignatureAppearance(iSignReason, iSignContact, iSignLocation, visible, stampFile,
+                await using Stream inputPdfDocument = new MemoryStream(fileToSign);
+                using PdfReader documentPdfReader = new PdfReader(inputPdfDocument);
+                using PdfDocument pdfDocument = new PdfDocument(documentPdfReader);
+                var pageSize = pdfDocument.GetFirstPage().GetPageSize();
+                await ConfigureSignatureAppearance(pageSize, iSignReason, iSignContact, iSignLocation, visible, stampFile,
                     certificate, pdfSigner);
 
                 _logger.Log(LogLevel.Information, "Loading private key and signature data...");
@@ -97,13 +101,14 @@ namespace PdfLittleSigner
             {
                 await _outputPdfStream.DisposeAsync();
             }
+
             _logger.Log(LogLevel.Information, "Signing pdf successful.");
             return true;
         }
 
         private IExternalSignature CreateExternalSignature(X509Certificate2 certificate)
         {
-            var hashAlgorithm = DigestAlgorithms.SHA1;
+            var hashAlgorithm = DigestAlgorithms.SHA256;
             var rsa = certificate.GetRSAPrivateKey();
             if (rsa == null)
             {
@@ -128,11 +133,12 @@ namespace PdfLittleSigner
                 dp,
                 dq,
                 inverseQ);
+
             IExternalSignature signature = new PrivateKeySignature(privateKey, hashAlgorithm);
             return signature;
         }
 
-        private async Task ConfigureSignatureAppearance(string iSignReason, string iSignContact, string iSignLocation,
+        private async Task ConfigureSignatureAppearance(Rectangle pageSize, string iSignReason, string iSignContact, string iSignLocation,
             bool visible, IFormFile stampFile, X509Certificate2 certificate, iText.Signatures.PdfSigner pdfSigner)
         {
             var signatureAppearance = pdfSigner.GetSignatureAppearance();
@@ -141,14 +147,20 @@ namespace PdfLittleSigner
                 .SetContact(iSignContact)
                 .SetLocation(iSignLocation);
 
-
             if (visible)
             {
-                signatureAppearance.SetPageRect(new iText.Kernel.Geom.Rectangle(_imageLocation.Width,
-                    _imageLocation.Height, _imageSize.Width, _imageSize.Height));
+                var signatureLocationX = pageSize.GetWidth() - _imageSize.Width - _stampMargin;
+                var signatureLocationY = pageSize.GetHeight() - _imageSize.Height - _stampMargin;
+                signatureAppearance.SetPageRect(new Rectangle(signatureLocationX, signatureLocationY, _imageSize.Width, _imageSize.Height));
 
                 if (stampFile != null)
                 {
+                    var stampImageExtension = Path.GetExtension(stampFile.FileName);
+                    if (Array.Exists(_unsupportedImageFormats, element => element.Equals(stampImageExtension)))
+                    {
+                        throw new Exception($"{stampImageExtension} image files are not supported");
+                    }
+
                     signatureAppearance.SetRenderingMode(PdfSignatureAppearance.RenderingMode.GRAPHIC);
                     var stampBytes = await FormFileToByteArrayAsync(stampFile);
                     ImageData imageData = ImageDataFactory.Create(stampBytes);
@@ -171,7 +183,6 @@ namespace PdfLittleSigner
         private iText.Signatures.PdfSigner GetPdfSigner(PdfReader pdfReader)
         {
             StampingProperties stampingProperties = new StampingProperties();
-
             iText.Signatures.PdfSigner pdfSigner = new(pdfReader, _outputPdfStream, stampingProperties);
             pdfSigner.SetSignDate(DateTime.Now);
             pdfSigner.SetCertificationLevel(iText.Signatures.PdfSigner.CERTIFIED_NO_CHANGES_ALLOWED);
@@ -185,6 +196,7 @@ namespace PdfLittleSigner
             var certRawData = cert.RawData;
             var certificates = cp.ReadCertificate(certRawData);
             Org.BouncyCastle.X509.X509Certificate[] chain = { certificates };
+
             return chain;
         }
 
@@ -192,6 +204,7 @@ namespace PdfLittleSigner
         {
             using var stream = new MemoryStream();
             await file.CopyToAsync(stream);
+
             return stream.ToArray();
         }
     }
